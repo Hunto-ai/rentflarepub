6681f4b1a649960dc066b9cd1f5acc1ba21797a8
Hunto-ai <131227572+Hunto-ai@users.noreply.github.com>
2024-04-10 11:49:41 -0600

    .

--- axios/api.js
+++ axios/api.js
@@ -1,7 +1,19 @@
 import axios from 'axios';
 
+const getBaseURL = () => {
+  if (process.env.NEXT_PUBLIC_PROD_STATE === 'true') {
+    return process.env.NEXT_PUBLIC_PROD_API_URL; 
+  } else if (process.env.NEXT_PUBLIC_PROD_STATE === 'false') {
+    return process.env.NEXT_PUBLIC_PREVIEW_API_URL;
+  } else {
+    // Default (development or if variables aren't set)
+    return 'http://localhost:5000';
+  }
+};
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: getBaseURL(),
+  withCredentials: true,
 });
 
 export default api;
